db489c6ec873587684c60b60fe0ab047e03b64fc
nyel-101 <dandevjr@gmail.com>
2025-02-24 11:28:49 +0000

    feat(worker): restructure payload parser

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f5fa855fbbf4df5b2153dcf98bcd01d = () => {
-  return '2025-02-20T11:30:27Z';
+const node_4af2a571ba1342229b975fa45145944f = () => {
+  return '2025-02-24T11:28:49Z';
 };
