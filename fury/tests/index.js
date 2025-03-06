2ebf906e68c458cc7a1455b04ea32a8b01325c9d
nyel-101 <dandevjr@gmail.com>
2025-03-06 19:30:25 +0000

    fix(utils): refactor caching layer

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec77d3e257064999878ca391a7d0d696 = () => {
-  return '2025-02-24T04:33:40Z';
+const node_58de3d675a5f4e9cb48e0f92285dd3a1 = () => {
+  return '2025-03-06T19:30:25Z';
 };
