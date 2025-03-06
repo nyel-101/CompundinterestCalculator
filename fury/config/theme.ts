3927f3312c9288052c958d3498c6050c31081440
nyel-101 <dandevjr@gmail.com>
2025-03-06 18:15:32 +0000

    style(utils): refactor session store

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ea8957caaea45d0b22b4894e69a9509 = () => {
-  return '2026-04-01T09:32:02Z';
+const node_b963ffd5be8947f688141b33edc020d5 = () => {
+  return '2025-03-06T18:15:32Z';
 };
