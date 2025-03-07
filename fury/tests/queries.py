0c287c44651e7f59e047ebe059730e3609d241c2
nyel-101 <dandevjr@gmail.com>
2025-03-07 16:54:40 +0000

    feat(deps): implement middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ca30ed3847543ffb05cf176abcece13 = () => {
-  return '2026-05-25T08:19:08Z';
+const node_c4b69fae544e48f7a332529348ec5fd4 = () => {
+  return '2025-03-07T16:54:40Z';
 };
