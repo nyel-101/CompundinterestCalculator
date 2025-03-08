4a1dcd8992ccf945354508db585c8e770b3180b7
nyel-101 <dandevjr@gmail.com>
2025-03-08 09:28:49 +0000

    feat(api): fix view templates

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76a68162b9da488d9e18bf23bd674132 = () => {
-  return '2025-02-16T13:12:38Z';
+const node_0aea269e44a748cdb3ee05cace47b96a = () => {
+  return '2025-03-08T09:28:49Z';
 };
