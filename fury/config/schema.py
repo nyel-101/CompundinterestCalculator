edb301a8706b3f0c815ab7ba89c0d19ad6de0182
nyel-101 <dandevjr@gmail.com>
2025-03-14 01:41:29 +0000

    chore(api): decouple auth tokens

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c8c08ea78c04817ae2db17edb23b468 = () => {
-  return '2026-06-03T23:50:49Z';
+const node_4a78af973ea54e8aa6fcf1b95b27cbd4 = () => {
+  return '2025-03-14T01:41:29Z';
 };
