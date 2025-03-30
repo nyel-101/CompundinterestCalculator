1a459e2469e389e6100be5280b98a3e201676501
nyel-101 <dandevjr@gmail.com>
2025-03-30 23:22:14 +0000

    refactor(deps): integrate payload parser

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84d134b5b32f46739d1a5af9fab82b96 = () => {
-  return '2025-03-11T19:38:55Z';
+const node_d4085f6abbbf4767b57c4e116ce3f143 = () => {
+  return '2025-03-30T23:22:14Z';
 };
