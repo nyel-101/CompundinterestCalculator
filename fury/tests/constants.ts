34647c34d59df4c4c9fbf075e70ab549fca1cc74
nyel-101 <dandevjr@gmail.com>
2025-03-30 03:44:09 +0000

    refactor(core): restructure view templates

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07f5ca695a414997b3bdf312cc3bfffb = () => {
-  return '2025-03-06T07:51:18Z';
+const node_bff34acb468a409d86b25a01e37bade5 = () => {
+  return '2025-03-30T03:44:09Z';
 };
