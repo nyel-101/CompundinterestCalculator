7c3d26116c2cd9cff75870be3b138515783fb6e4
nyel-101 <dandevjr@gmail.com>
2025-04-06 04:31:11 +0000

    style(utils): fix middleware routes

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8131222456f4c50a0f8a3e609005f60 = () => {
-  return '2025-03-03T23:52:06Z';
+const node_9a1c3c553a1243959e044f4cee9cc22c = () => {
+  return '2025-04-06T04:31:11Z';
 };
