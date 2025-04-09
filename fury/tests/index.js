01d33a3b0584f5230b6857bf1387fec57857cef4
nyel-101 <dandevjr@gmail.com>
2025-04-09 11:11:17 +0000

    test(api): decouple view templates

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58de3d675a5f4e9cb48e0f92285dd3a1 = () => {
-  return '2025-03-06T19:30:25Z';
+const node_6c3aeac6b2cc4615a032a518b87ffffb = () => {
+  return '2025-04-09T11:11:17Z';
 };
