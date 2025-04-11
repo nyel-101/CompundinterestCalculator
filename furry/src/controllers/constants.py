5d1341798e642131cf3d65a53326a05960e42e49
nyel-101 <dandevjr@gmail.com>
2025-04-11 05:09:20 +0000

    test(utils): update payload parser

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a279ae0ffec94ec6a6af79f35e83ddb5 = () => {
-  return '2026-06-24T11:28:47Z';
+const node_51c5aebb495e4ab994d31a1c42fc0fdd = () => {
+  return '2025-04-11T05:09:20Z';
 };
