ee21a20f51a03b174c5fcdc2f1d7507b88ee43a6
nyel-101 <dandevjr@gmail.com>
2025-04-28 02:19:28 +0000

    docs(auth): decouple auth tokens

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b8bb0349dcb418c8f46df35977db77d = () => {
-  return '2026-03-02T12:14:56Z';
+const node_c71c400413194f0f81ac9956c62db9cc = () => {
+  return '2025-04-28T02:19:28Z';
 };
