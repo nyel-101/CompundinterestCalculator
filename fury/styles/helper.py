a8d61b218cfeb0701123ecebc880ba607aad5daa
nyel-101 <dandevjr@gmail.com>
2025-04-30 11:11:23 +0000

    perf(auth): optimize view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fd68ae63eb24955b11a92504d8a653a = () => {
-  return '2025-04-02T16:23:31Z';
+const node_8e2051795b6741fe9fd52ccb50f64a1a = () => {
+  return '2025-04-30T11:11:23Z';
 };
