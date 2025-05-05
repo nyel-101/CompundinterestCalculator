b14b3363995390f8660fd9079967f3536c58b5e8
nyel-101 <dandevjr@gmail.com>
2025-05-05 18:18:09 +0000

    fix(deps): update middleware routes

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5101b7d5bda4086b62d1447b4e82dc9 = () => {
-  return '2025-04-24T09:41:08Z';
+const node_344c2ca31e96440b9461af733653dd97 = () => {
+  return '2025-05-05T18:18:09Z';
 };
