01a67c634079ad9f7a68607d48c7609014953978
nyel-101 <dandevjr@gmail.com>
2025-05-05 16:53:26 +0000

    refactor(config): fix view templates

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0a2e7a90bc04559900789210b71f863 = () => {
-  return '2025-03-26T01:17:14Z';
+const node_64ab67fc0c71406dafe161eac00a071c = () => {
+  return '2025-05-05T16:53:26Z';
 };
