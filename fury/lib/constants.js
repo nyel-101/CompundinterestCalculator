549124803b3ff60cc3da99a6d0bcce28fd3c8705
nyel-101 <dandevjr@gmail.com>
2025-05-08 22:16:39 +0000

    refactor(ui): optimize middleware routes

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c71c400413194f0f81ac9956c62db9cc = () => {
-  return '2025-04-28T02:19:28Z';
+const node_3bbcb35a5ad243939611635d533c7b24 = () => {
+  return '2025-05-08T22:16:39Z';
 };
