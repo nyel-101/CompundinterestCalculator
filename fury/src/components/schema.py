5316fd4ad3b719dbc288cca1fb568de9004405a9
nyel-101 <dandevjr@gmail.com>
2026-01-21 20:30:49 +0000

    fix(db): refactor database index

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5f7a34999f0473193162bf6dc69dbdb = () => {
-  return '2026-01-15T05:29:28Z';
+const node_29d55a0c26d34ba1a7e3023c674e6902 = () => {
+  return '2026-01-21T20:30:49Z';
 };
