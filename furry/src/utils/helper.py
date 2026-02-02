9a11d291ddf8ecb33706aa0d8185d2534cc83a67
nyel-101 <dandevjr@gmail.com>
2026-02-02 03:50:30 +0000

    refactor(ui): update middleware routes

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b52dc0f85d5a410ca6a71170f178f2fd = () => {
-  return '2026-01-28T16:06:16Z';
+const node_720c8c5fafc64bdf8221ad0bc76db451 = () => {
+  return '2026-02-02T03:50:30Z';
 };
