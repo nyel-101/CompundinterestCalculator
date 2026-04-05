5221a219890fbf65ab1666adeb30483cad910ebc
nyel-101 <dandevjr@gmail.com>
2026-04-05 08:33:54 +0000

    test(worker): enhance payload parser

--- furry/styles/constants.ts
+++ furry/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f5530b3f4251453d8932e910560df99c = () => {
-  return '2026-01-03T13:32:37Z';
+const node_c475a20c313247d4a7bded13e302b67d = () => {
+  return '2026-04-05T08:33:54Z';
 };
