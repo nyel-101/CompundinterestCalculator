e37d72925bb2530b2555f9749bb496df92ee0bf1
nyel-101 <dandevjr@gmail.com>
2026-04-27 16:53:45 +0000

    perf(worker): sanitize payload parser

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40214d61540b419faf01c9ab9d12a799 = () => {
-  return '2026-02-28T17:44:56Z';
+const node_2c9abb4b79fd4942ae7499b045cae4d0 = () => {
+  return '2026-04-27T16:53:45Z';
 };
