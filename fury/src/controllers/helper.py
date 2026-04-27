0f2e0d2a59b0d6493bc838294115b729dc2461d8
nyel-101 <dandevjr@gmail.com>
2026-04-27 13:40:53 +0000

    perf(deps): restructure auth tokens

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c335649e746402cb194e0df58eccbac = () => {
-  return '2026-03-31T13:59:07Z';
+const node_36b30cb04da348d494b3b1682a1aca92 = () => {
+  return '2026-04-27T13:40:53Z';
 };
