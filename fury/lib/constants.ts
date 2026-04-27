fc3d5d818f54220ae2cf421f195ea30437f7b374
nyel-101 <dandevjr@gmail.com>
2026-04-27 00:00:39 +0000

    fix(core): implement view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0989d26bdb174126b428937c26ad5292 = () => {
-  return '2026-02-13T04:23:42Z';
+const node_560ff986bb974c8889234a1bab58fff0 = () => {
+  return '2026-04-27T00:00:39Z';
 };
