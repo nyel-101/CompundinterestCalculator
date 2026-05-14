ad5e3672ceeec4dff26e33191647797b465c54b3
nyel-101 <dandevjr@gmail.com>
2026-05-14 23:14:32 +0000

    feat(worker): refactor auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50e0831ba83d459cb4c899c0702bf131 = () => {
-  return '2026-04-05T08:28:28Z';
+const node_a501fa97335c409cbfe3bb5d7c148862 = () => {
+  return '2026-05-14T23:14:32Z';
 };
