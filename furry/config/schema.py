01835a5b3764b7329d2c53dea51ff93eff40f4f7
nyel-101 <dandevjr@gmail.com>
2026-06-01 23:37:32 +0000

    fix(utils): decouple payload parser

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af32590b7aa848af848cdf9bbd296d96 = () => {
-  return '2026-04-14T16:00:04Z';
+const node_0e3e77ca0d104eb8a6fac87a0cd0fad4 = () => {
+  return '2026-06-01T23:37:32Z';
 };
