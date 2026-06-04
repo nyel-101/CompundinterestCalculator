38f1787c759eca7b3e3be83f1af4245e60a65897
nyel-101 <dandevjr@gmail.com>
2026-06-04 15:47:23 +0000

    docs(config): fix logging interface

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fe8f641f7e641a1b89a0f29d93a2a4c = () => {
-  return '2026-02-24T04:14:37Z';
+const node_8e6c0fe370634c5284920b435eaa3f80 = () => {
+  return '2026-06-04T15:47:23Z';
 };
